912ae253a8a716804ac6c3a473709d2e18fd9cb8
Mohamed-Abukhris <mohamed.s.abukhris@gmail.com>
2025-11-06 16:48:59 +0200

    feat(auth): add Firebase admin and client setup with auth and storage actions

---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,155 @@
+"use server";
+
+import { db, auth } from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+const ONE_WEEK = 60 * 60 * 24 * 7;
+
+type FirebaseAuthError = {
+  code?: string;
+};
+
+export async function signUp(params: SignUpParams) {
+  const { uid, name, email } = params;
+
+  try {
+    // check if user exists in db
+    const userRecord = await db.collection("users").doc(uid).get();
+    if (userRecord.exists)
+      return {
+        success: false,
+        message: "User already exists. Please sign in.",
+      };
+
+    // save user to db
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+      // profileURL,
+      // resumeURL,
+    });
+
+    return {
+      success: true,
+      message: "Account created successfully. Please sign in.",
+    };
+  } catch (error: unknown) {
+    // eslint-disable-next-line no-console
+    console.error("Error creating user:", error);
+
+    // Handle Firebase specific errors (without using `any`)
+    if (typeof error === "object" && error !== null && "code" in error) {
+      const { code } = error as FirebaseAuthError;
+
+      if (code === "auth/email-already-exists") {
+        return {
+          success: false,
+          message: "This email is already in use",
+        };
+      }
+    }
+
+    return {
+      success: false,
+      message: "Failed to create account. Please try again.",
+    };
+  }
+}
+
+export async function signIn(params: SignInParams) {
+  const { email, idToken } = params;
+
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord) {
+      return {
+        success: false,
+        message: "User does not exist. Create an account.",
+      };
+    }
+
+    await setSessionCookie(idToken);
+
+    return {
+      success: true,
+      message: "Signed in successfully.",
+    };
+  } catch (error: unknown) {
+    // eslint-disable-next-line no-console
+    console.error("Error signing in:", error);
+
+    // handle Firebase-specific errors safely
+    if (typeof error === "object" && error !== null && "code" in error) {
+      const { code } = error as FirebaseAuthError;
+
+      if (code === "auth/invalid-credential") {
+        return {
+          success: false,
+          message: "Invalid email or password.",
+        };
+      }
+
+      if (code === "auth/user-not-found") {
+        return {
+          success: false,
+          message: "No user found with this email.",
+        };
+      }
+    }
+
+    return {
+      success: false,
+      message: "Failed to log into account. Please try again.",
+    };
+  }
+}
+
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK * 1000,
+  });
+
+  cookieStore.set("session", sessionCookie, {
+    maxAge: ONE_WEEK,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
+}
+
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
+
+  const sessionCookie = cookieStore.get("session")?.value;
+  if (!sessionCookie) return null;
+
+  try {
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+    // get user info from db
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
+    if (!userRecord.exists) return null;
+
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+
+    // Invalid or expired session
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+  return !!user;
+}
