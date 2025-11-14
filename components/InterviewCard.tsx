8f9c75cb425ac8d470a2a29f33b501ac9508f8a9
Mohamed-Abukhris <mohamed.s.abukhris@gmail.com>
2025-11-15 00:15:44 +0200

    feat(mvp): add feedback page for interview session and update related components
    - Added new route: app/(root)/interview/[id]/feedback/page.tsx
    - Updated interview and feedback pages for improved flow
    - Enhanced Agent and InterviewCard components integration

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -4,8 +4,9 @@ import { getRandomInterviewCover } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import DisplayTechIcons from "@/components/DisplayTechIcons";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
 
-const InterviewCard = ({
+const InterviewCard = async ({
   id,
   userId,
   role,
@@ -13,19 +14,23 @@ const InterviewCard = ({
   techstack,
   createdAt,
 }: InterviewCardProps) => {
-  const feedback = null as Feedback | null;
+  const feedback =
+    userId && id
+      ? await getFeedbackByInterviewId({ interviewId: id, userId })
+      : null;
   const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
   const formattedDate = dayjs(
     feedback?.createdAt || createdAt || Date.now(),
-  ).format("DD-MM-YYYY");
+  ).format("MMM D, YYYY");
 
   return (
     <div className="card-border w-[360px] max-sm:w-full min-h-96">
       <div className="card-interview">
         <div>
-          <div className="absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg">
+          <div className="absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600">
             <p className="badge-text">{normalizedType}</p>
           </div>
+
           <Image
             src={getRandomInterviewCover()}
             alt="cover image"
@@ -33,7 +38,9 @@ const InterviewCard = ({
             height={90}
             className="rounded-full object-fit size-[90px]"
           />
+
           <h3 className="mt-5 capitalize">{role} Interview</h3>
+
           <div className="flex flex-row gap-5 mt-3">
             <div className="flex flex-row gap-2">
               <Image
@@ -44,18 +51,22 @@ const InterviewCard = ({
               />
               <p>{formattedDate}</p>
             </div>
+
             <div className="flex flex-row gap-2 items-center">
               <Image src="/star.svg" alt="star" width={22} height={22} />
               <p>{feedback?.totalScore || "---"}/100</p>
             </div>
           </div>
+
           <p className="line-clamp-2 mt-5">
             {feedback?.finalAssessment ||
               "You haven't taken the interview yet. Take it now to improve your skills."}
           </p>
         </div>
+
         <div className="flex flex-row justify-between">
           <DisplayTechIcons techStack={techstack} />
+
           <Button className="btn-primary">
             <Link
               href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}
